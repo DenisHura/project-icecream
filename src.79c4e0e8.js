3c0f0703a3e95d6e8a085eb0193ffa20ca3a3eaf
DenisHura <92687619+DenisHura@users.noreply.github.com>
2022-01-16 20:33:32 +0000

    Deploying to gh-pages from @ DenisHura/project-icecream@2cb9e6c089f88f9a54a466e980bb643703d1ac08 🚀

--- src.79c4e0e8.js
+++ src.79c4e0e8.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-icecream/src.62b44170.js.map
+//# sourceMappingURL=/project-icecream/src.79c4e0e8.js.map
